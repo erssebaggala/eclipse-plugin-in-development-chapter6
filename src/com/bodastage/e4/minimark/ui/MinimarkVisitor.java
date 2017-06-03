663194d819a487b267b85b01580867c1a426a461
Bodastage <bodastage@gmail.com>
2017-06-03 17:07:08 +0300

    Iterating through resources

--- src/com/bodastage/e4/minimark/ui/MinimarkVisitor.java
+++ src/com/bodastage/e4/minimark/ui/MinimarkVisitor.java
@@ -1,10 +1,22 @@
 package com.bodastage.e4.minimark.ui;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.resources.IResourceProxy;
 import org.eclipse.core.resources.IResourceProxyVisitor;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
 
 public class MinimarkVisitor implements IResourceProxyVisitor, IResourceDeltaVisitor {
 
@@ -18,9 +30,23 @@ public class MinimarkVisitor implements IResourceProxyVisitor, IResourceDeltaVis
 		String name = proxy.getName();
 		if (name != null && name.endsWith(".minimark")) {
 			// found a source file
-			System.out.println("Processing " + name);
+			//System.out.println("Processing " + name);
+			processResource(proxy.requestResource());
 		}
 		return true;
+	}
+
+	private void processResource(IResource resource) throws CoreException {
+		if (resource instanceof IFile) {
+			try {
+				IFile file = (IFile) resource;
+				InputStream in = file.getContents();
+				MinimarkTranslator.convert(new InputStreamReader(in), new OutputStreamWriter(System.out));
+			} catch (IOException e) {
+				throw new CoreException(
+						new Status(Status.ERROR, Activator.PLUGIN_ID, "Failed to generate resource", e));
+			}
+		}
 	}
 
 }
