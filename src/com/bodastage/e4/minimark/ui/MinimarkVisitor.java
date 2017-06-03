a84102927cb86866396fecfb21272e3607da014b
Bodastage <bodastage@gmail.com>
2017-06-04 02:16:41 +0300

    Handling deletion

--- src/com/bodastage/e4/minimark/ui/MinimarkVisitor.java
+++ src/com/bodastage/e4/minimark/ui/MinimarkVisitor.java
@@ -22,9 +22,26 @@ public class MinimarkVisitor implements IResourceProxyVisitor, IResourceDeltaVis
 
 	@Override
 	public boolean visit(IResourceDelta delta) throws CoreException {
+		boolean deleted = (IResourceDelta.REMOVED & delta.getKind()) != 0;
 		IResource resource = delta.getResource();
-		if (resource.getName().endsWith(".minimark")) {
-			processResource(resource);
+		String name = resource.getName();
+
+		if (name.endsWith(".minimark")) {
+			if (deleted) {
+				String htmlName = name.replace(".minimark", ".html");
+				IFile htmlFile = resource.getParent().getFile(new Path(htmlName));
+				if (htmlFile.exists()) {
+					htmlFile.delete(true, null);
+				}
+			} else {
+				processResource(resource);
+			}
+		} else if (name.endsWith(".html")) {
+			String minimarkName = name.replace(".html", ".minimark");
+			IFile minimarkFile = resource.getParent().getFile(new Path(minimarkName));
+			if (minimarkFile.exists()) {
+				processResource(minimarkFile);
+			}
 		}
 		return true;
 	}
@@ -33,33 +50,35 @@ public class MinimarkVisitor implements IResourceProxyVisitor, IResourceDeltaVis
 	public boolean visit(IResourceProxy proxy) throws CoreException {
 		String name = proxy.getName();
 		if (name != null && name.endsWith(".minimark")) {
-			// found a source file
-			// System.out.println("Processing " + name);
 			processResource(proxy.requestResource());
 		}
 		return true;
 	}
 
-	private void processResource(IResource resource) {
-		try {
-			IFile file = (IFile) resource;
-			InputStream in = file.getContents();
-			String htmlName = file.getName().replace(".minimark", ".html");
-			IContainer container = file.getParent();
-			IFile htmlFile = container.getFile(new Path(htmlName));
+	private void processResource(IResource resource) throws CoreException {
 
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			MinimarkTranslator.convert(new InputStreamReader(in), new OutputStreamWriter(baos));
-			ByteArrayInputStream contents = new ByteArrayInputStream(baos.toByteArray());
+		if (resource instanceof IFile && resource.exists()) {
+			try {
+				IFile file = (IFile) resource;
+				InputStream in = file.getContents();
+				String htmlName = file.getName().replace(".minimark", ".html");
+				IContainer container = file.getParent();
+				IFile htmlFile = container.getFile(new Path(htmlName));
 
-			if (htmlFile.exists()) {
-				htmlFile.setContents(contents, true, false, null);
-			} else {
-				htmlFile.create(contents, true, null);
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				MinimarkTranslator.convert(new InputStreamReader(in), new OutputStreamWriter(baos));
+				ByteArrayInputStream contents = new ByteArrayInputStream(baos.toByteArray());
+
+				if (htmlFile.exists()) {
+					htmlFile.setContents(contents, true, false, null);
+				} else {
+					htmlFile.create(contents, true, null);
+				}
+				htmlFile.setDerived(true, null);
+
+			} catch (Exception e) {
+
 			}
-			htmlFile.setDerived(true, null);
-		} catch (Exception e) {
-
 		}
 	}
 
