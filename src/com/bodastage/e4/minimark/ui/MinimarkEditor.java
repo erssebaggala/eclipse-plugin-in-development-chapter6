b3296da556fbed16ea6f323a3d11386236c9c101
Bodastage <bodastage@gmail.com>
2017-06-03 15:36:55 +0300

    Creating an editor

---
+++ src/com/bodastage/e4/minimark/ui/MinimarkEditor.java
@@ -0,0 +1,10 @@
+package com.bodastage.e4.minimark.ui;
+
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+
+public class MinimarkEditor extends AbstractTextEditor {
+	public MinimarkEditor() {
+		setDocumentProvider(new TextFileDocumentProvider());
+		}
+}
